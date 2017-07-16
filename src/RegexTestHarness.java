9a654c7c9534f09982be169a6826610b6a92f9e8
Unknown <kanabar1@gmail.com>
2017-07-16 14:38:29 +0100

    Add examples
    Added some examples to RegExTestHarness class

--- src/RegexTestHarness.java
+++ src/RegexTestHarness.java
@@ -8,7 +8,11 @@ public class RegexTestHarness
 {
     public static void main(String[] args)
     {
-        
+        String longString = "Derek Banas CA 12345 PA (412)555-1212 johnsmith@hotmail.com 412-555-1234 412 555-1234";
+        String strangeString = " 1Z aaa **** *** {{{ {{ {";
+
+        //regExChecker("A[A-Za-z]|C[A-Za-z]",longString);
+        //regExChecker("(\\{{1,})",strangeString);
     }
 
     public static void regExChecker(String theRegEx, String string2Check)
